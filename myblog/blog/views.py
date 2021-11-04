1e460d04aa49a37f7256641a7a872e5531c486f5
Oumarou Altine Mohamadou Aminou <oumaroulamine98@gmail.com>
2021-11-04 01:39:45 +0100

    creation de blog avec django: 2eme partie

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
+
+def post_list(request):
+    posts = Post.objects.all()
+    print(posts)
+    return render(request,'blog/post/list.html',{'posts':posts})
+
+def post_detail(request, slug: str):
+    try:
+        post = Post.objects.get(slug=slug)
+    except Post.DoesNotExist:
+        raise('This post doesnot exist')
+    return render(request,'blog/post/detail.html',{'post':post})
